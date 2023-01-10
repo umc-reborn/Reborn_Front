461ed2daed4d7fe5b6f33c2c8ae9e9a55a59cff9
JaeKyu Park <pjk971113@naver.com>
2023-01-10 18:07:05 +0900

    base file update

---
+++ UMC-Reborn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC-Reborn
+//
+//  Created by jaegu park on 2023/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
