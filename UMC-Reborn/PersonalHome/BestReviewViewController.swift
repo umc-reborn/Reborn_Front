763b32a19289bd1869824ddc119fe4e4fa2047db
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-01-18 07:55:11 +0900

    [style]Home 화면 레이아웃수정 및 파일추가

---
+++ UMC-Reborn/PersonalHome/BestReviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BestReviewViewController.swift
+//  UMC-Reborn
+//
+//  Created by nayeon  on 2023/01/18.
+//
+
+import UIKit
+
+class BestReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ UMC-Reborn/PersonalHome/PopularShopViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopularShopViewController.swift
+//  UMC-Reborn
+//
+//  Created by nayeon  on 2023/01/18.
+//
+
+import Tabman
+import Pageboy
+
+class PopularShopViewController: TabmanViewController {
+
+    private var viewControllers: Array<UIViewController> = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
