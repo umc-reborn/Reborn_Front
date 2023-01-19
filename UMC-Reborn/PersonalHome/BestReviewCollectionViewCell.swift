ff695d0692d551b2706d2bbbf92d9260bbe47b17
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-01-20 04:03:18 +0900

    [Feat]모집중 가게 및 새로운 가게 collectionView 구현

---
+++ UMC-Reborn/PersonalHome/BestReviewCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  BestReviewCollectionViewCell.swift
+//  UMC-Reborn
+//
+//  Created by nayeon  on 2023/01/20.
+//
+
+import UIKit
---
+++ UMC-Reborn/PersonalHome/NewShopCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  NewShopCollectionViewCell.swift
+//  UMC-Reborn
+//
+//  Created by nayeon  on 2023/01/20.
+//
+
+import UIKit
+
+class NewShopCollectionViewCell:UICollectionViewCell {
+    
+    @IBOutlet weak var shopName: UILabel!
+    @IBOutlet weak var shopImage: UIImageView!
+    @IBOutlet weak var shopLocation: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+        
+    var likeBool: Bool = false {
+        willSet(newValue) {
+            if newValue {
+                likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+                likeButton.tintColor = UIColor.red
+            }
+            else {
+                likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                likeButton.tintColor = UIColor.lightGray
+            }
+        }
+    }
+    @IBAction func likeButtonAction(_ sender: UIButton) {
+        if likeBool {
+            likeBool = false
+//            removeDataAcion()
+        }
+        else {
+            likeBool = true
+//            pullDataAcion()
+        }
+    }
+    
+}
