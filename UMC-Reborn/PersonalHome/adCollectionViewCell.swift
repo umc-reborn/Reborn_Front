fcbea5b49fa82b4ebda8ef74d4beb9c7427867ce
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-01-23 02:22:41 +0900

    [Feat]ad 화면 구현

---
+++ UMC-Reborn/PersonalHome/adCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  adCollectionViewCell.swift
+//  UMC-Reborn
+//
+//  Created by nayeon  on 2023/01/23.
+//
+
+import UIKit
+
+class adCollectionViewCell:UICollectionViewCell {
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var adImage: UIImageView!
+}
