658a919692c32a00364301e6d31e2bf06209289f
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-01-31 13:18:31 +0900

    [Feat]검색 정렬 버튼 생성

--- UMC-Reborn/SearchVC/SearchResultViewController.swift
+++ UMC-Reborn/SearchVC/SearchResultViewController.swift
@@ -6,18 +6,28 @@
 //
 
 import UIKit
+import DropDown
 
 class SearchResultViewController: UIViewController {
     
     var shopList: [String] = ["베이커리","어쩌구","저쩌구","베이커리","어쩌구","저쩌구","베이커리","어쩌구","저쩌구","베이커리","어쩌구","저쩌구"]
     var ratingNum: [String] = ["5.0","4.5","3.2","5.0","4.5","3.2","5.0","4.5","3.2","5.0","4.5","3.2"]
-    
+    @IBOutlet weak var tfInput: UITextField!
+    @IBOutlet weak var ivIcon: UIImageView!
+    @IBOutlet weak var btnSelect: UIButton!
+    @IBOutlet weak var dropView: UIView!
     @IBOutlet weak var ResultTableView: UITableView!
+    
+    let dropdown = DropDown()
+    let itemList = ["이름순","별점순","인기순"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(named: "Background")
 
         setSearchBar()
+        initUI()
+        setDropdown()
         
     }
     
@@ -30,8 +40,44 @@ class SearchResultViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: searchBar)
             
         }
-        
     
+    func initUI(){
+        dropView.backgroundColor = UIColor.white
+        dropView.layer.cornerRadius = 10
+        
+        DropDown.appearance().textColor = UIColor.black
+        DropDown.appearance().selectedTextColor = UIColor.red
+        DropDown.appearance().backgroundColor = UIColor.white
+        DropDown.appearance().selectionBackgroundColor = UIColor.lightGray
+        DropDown.appearance().setupCornerRadius(10)
+        dropdown.dismissMode = .automatic
+        
+        ivIcon.tintColor = UIColor.gray
+        tfInput.text = "정렬"
+    }
+    
+    func setDropdown(){
+        dropdown.dataSource = itemList
+        
+        dropdown.anchorView = self.dropView
+        
+        dropdown.bottomOffset = CGPoint(x: 0, y: dropView.bounds.height)
+        dropdown.selectionAction = { [weak self] (index, item) in
+            self!.tfInput.text = item
+            self!.ivIcon.image = UIImage.init(named: "chevron.down")
+        }
+        
+        dropdown.cancelAction = {[weak self] in
+            self?.ivIcon.image = UIImage.init(named: "chevron.down")
+        }
+    }
+    
+    @IBAction func dropdownClicked(_ sender: Any){
+        dropdown.show()
+        self.ivIcon.image = UIImage.init(named: "chevron.up")
+        ivIcon.tintColor = UIColor.red
+    }
+        
 }
 
 // MARK: - Extensions
