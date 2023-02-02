f8101da1a4ff7ed07178027fe3a67c77d874c810
yeonsu <drawingbonny@gmail.com>
2023-02-02 19:30:12 +0900

    feat: Add ChangePassword ViewController

---
+++ UMC-Reborn/MyReborn/ChangePasswordViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChangePasswordViewController.swift
+//  UMC-Reborn
+//
+//  Created by yeonsu on 2023/02/02.
+//
+
+import UIKit
+
+class ChangePasswordViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- UMC-Reborn/MyReborn/RebornHistoryViewController.swift
+++ UMC-Reborn/MyReborn/RebornHistoryViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class RebornHistoryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    // 메뉴 TableViewCell에 들어갈 데이터 모델
+    struct MyRebornMenuDataModel {
+        let MyRebornMenuIcon: UIImage?  // 메뉴 타이틀
+        let MyRebornMenuLabel: String   // 아이콘
+        let MyRebornNextIcon: UIImage?  // 화살표 아이콘
+    }
+    
+    // 메뉴 TableViewCell 데이터
+    let MyRebornMenu: [MyRebornMenuDataModel] = [MyRebornMenuDataModel(MyRebornMenuIcon: UIImage(named: "my"), MyRebornMenuLabel: "회원정보수정", MyRebornNextIcon: UIImage(named: "arrow")), MyRebornMenuDataModel(MyRebornMenuIcon: UIImage(named: "present"), MyRebornMenuLabel: "리본 히스토리", MyRebornNextIcon: UIImage(named: "arrow")), MyRebornMenuDataModel(MyRebornMenuIcon: UIImage(named: "write"), MyRebornMenuLabel: "리뷰 관리", MyRebornNextIcon: UIImage(named: "arrow")),]
     
     @IBOutlet weak var RebornHistoryTableView: UITableView!
     
