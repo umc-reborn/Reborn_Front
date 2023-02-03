e3cf79b9d702819c79467e598a04a98996a70cdb
yeonsu <drawingbonny@gmail.com>
2023-02-03 18:21:31 +0900

    feat: Add WriteReview api

---
+++ UMC-Reborn/MyReborn/Models/APIMyRebornPost.swift
@@ -0,0 +1,56 @@
+//
+//  APIMyRebornPost.swift
+//  UMC-Reborn
+//
+//  Created by yeonsu on 2023/02/03.
+//
+
+import Foundation
+import Alamofire
+
+struct postReviewReq:Encodable {
+    var userIdx:Int
+    var rebornIdx:String
+    var reviewScore:String
+    var reviewComment:String
+    
+}
+
+struct postReviewReqResultModel: Codable {
+    var isSuccess:Bool
+    var code:Int
+    var message:String
+    var result:RebornResult
+}
+
+struct postReviewReqResult: Codable {
+    var rebornIdx:Int
+}
+
+class APIMyRebornHandlerPost {
+    static let instance = APIMyRebornHandlerPost()
+    
+    func SendingPostReborn(parameters: RebornModel, handler: @escaping (_ result: [postReviewReqResultModel])->(Void)) {
+        let url = "http://www.rebornapp.shop/review"
+        let headers:HTTPHeaders = [
+            "content-type": "application/json"
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, encoder: JSONParameterEncoder.default, headers: headers).response { responce in
+            switch responce.result {
+            case .success(let data):
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .fragmentsAllowed)
+                    print(json)
+                    
+                    let jsonresult = try JSONDecoder().decode([postReviewReqResultModel].self, from: data!)
+                    handler(jsonresult)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
