b5d1886327aab2257386baa361f4155cc3e21ae1
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-02-07 22:33:39 +0900

    [Feat]카카오 지도 api 연결 및 기능 구현

--- UMC-Reborn/PersonalHomeVC/PersonalHomeViewController.swift
+++ UMC-Reborn/PersonalHomeVC/PersonalHomeViewController.swift
@@ -14,11 +14,54 @@ class PersonalHomeViewController: UIViewController {
     @IBOutlet weak var defaultLabel: UILabel!
     @IBOutlet weak var umcimg: UIImageView!
     @IBOutlet weak var backgroundView: UIView!
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    let button = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         addShadow()
+        contentView.addSubview(floatingButton)
+        
+        NSLayoutConstraint.activate([
+                   
+                   // 화면에 계속 보여지도록 frameLayout에 맞춰 floatingButton 추가
+                   floatingButton.bottomAnchor.constraint(equalTo: scrollView.frameLayoutGuide.bottomAnchor, constant: -20),
+                   floatingButton.trailingAnchor.constraint(equalTo: scrollView.frameLayoutGuide.trailingAnchor, constant: -10)
+               ])
     }
+    
+    let floatingButton: UIButton = {
+        let button = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .light)
+        let image = UIImage(systemName: "map", withConfiguration: imageConfig)
+        
+        button.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        // 버튼 넓이 300
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = UIColor(named: "MainColor")
+        button.layer.cornerRadius = 20
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(addButtonAction), for: .touchUpInside)
+//        button.setImage(UIImage(systemName: "map"), for: .normal)
+        return button
+    }()
+    
+    @objc func addButtonAction(_ sender: UIButton){
+//            self.selectedIndex = 700
+        let MapVC = UIStoryboard.init(name: "StoreMap", bundle: nil).instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+        
+//        MapVC.modalTransitionStyle = .coverVertical
+        MapVC.modalPresentationStyle = .automatic
+        
+        self.present(MapVC, animated: true, completion: nil)
+        }
+
+
     
     private func addShadow(){
         backgroundView.backgroundColor = .white
@@ -32,6 +75,7 @@ class PersonalHomeViewController: UIViewController {
         backgroundView.layer.masksToBounds = true
         backgroundView.layer.masksToBounds = false
     }
+    
 
 
 }
