56a02bb167d45fa30afbbff95accd38f52068dca
angryeon7 <87401351+angryeon7@users.noreply.github.com>
2023-02-21 22:19:05 +0900

    [Feat]좋아할만한 가게 기능 추가 및 검색 UI 수정

---
+++ UMC-Reborn/SearchVC/WillLikeShopCell.swift
@@ -0,0 +1,44 @@
+//
+//  WillLikeShopCell.swift
+//  UMC-Reborn
+//
+//  Created by nayeon  on 2023/02/21.
+//
+
+import Foundation
+import UIKit
+
+class WillLikeShopCell:UICollectionViewCell {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var shopName: UILabel!
+    @IBOutlet weak var shopImage: UIImageView!
+    @IBOutlet weak var shopLocation: UILabel!
+    @IBOutlet weak var shopScore: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
+    var likeBool: Bool = false {
+        willSet(newValue) {
+            if newValue {
+                likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+                likeButton.tintColor = UIColor.red
+            }
+            else {
+                likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                likeButton.tintColor = UIColor.lightGray
+            }
+        }
+    }
+    // MARK: - IBAction
+    @IBAction func likeButtonAction(_ sender: UIButton) {
+        if likeBool {
+            likeBool = false
+            //            removeDataAcion()
+        }
+        else {
+            likeBool = true
+            //            pullDataAcion()
+        }
+    }
+    
+}
