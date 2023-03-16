4b250a3b90c7fd3c36889551e08698010a5a114b
yeonsu <drawingbonny@gmail.com>
2023-03-16 23:49:29 +0900

    feat: Modify review API

---
+++ UMC-Reborn/MyReborn/ReviewManageTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewManageTableViewCell.swift
+//  UMC-Reborn
+//
+//  Created by yeonsu on 2023/03/15.
+//
+
+import UIKit
+
+class ReviewManageTableViewCell: UITableViewCell {
+    
+    let flowlayout = UICollectionViewFlowLayout()
+    
+    var reviewImageDatas : [ReviewManageModelResponse] = []
+
+    
+
+}
